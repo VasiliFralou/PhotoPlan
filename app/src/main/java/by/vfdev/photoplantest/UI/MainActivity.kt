1debaa88cc38b76f46b088c8b3473430ed51efba
VasiliFralou <Dr1fteR20021998>
2022-04-21 21:50:12 +0300

    Add MVVM Architecture

--- app/src/main/java/by/vfdev/photoplantest/UI/MainActivity.kt
+++ app/src/main/java/by/vfdev/photoplantest/UI/MainActivity.kt
@@ -1,7 +1,8 @@
-package by.vfdev.photoplantest
+package by.vfdev.photoplantest.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.vfdev.photoplantest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
