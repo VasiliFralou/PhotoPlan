c5a69c1a9c72e9a14007622a17ceab94ad10fff4
VasiliFralou <Dr1fteR20021998>
2022-04-22 22:24:57 +0300

    Add select image from gallery

--- app/src/main/java/by/vfdev/photoplantest/UI/MainActivity.kt
+++ app/src/main/java/by/vfdev/photoplantest/UI/MainActivity.kt
@@ -1,12 +1,40 @@
 package by.vfdev.photoplantest.UI
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import by.vfdev.photoplantest.R
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import by.vfdev.photoplantest.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAdd.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun openGallery() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            val value = data?.data
+            binding.imv.setImageURI(value)
+            binding.textView.text = value.toString()
+        }
     }
 }
